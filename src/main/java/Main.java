76a541099e4e71c85229c40a26e54861b24bcb40
Flooooooooooorian <florian.weber@neuefische.de>
2024-09-03 10:07:45 +0200

    Add hello

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,4 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello cgn-java-24-4");
     }
+
+    public static void hello() {
+        System.out.println("Hello");
+    }
 }
