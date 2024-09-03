83b7b33454fb2b62fa45e871f467b20c5cf029fb
Flooooooooooorian <florian.weber@neuefische.de>
2024-09-03 10:35:44 +0200

    Add tschau

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello cgn-java-24-4");
+        System.out.println("tschau");
     }
 
     public static void hello() {
